f9d2c0d0f7986b14625e965ea37678f0d49f15e7
Thomas Kreidl <vanthomiy@live.de>
2022-05-04 18:19:50 +0200

    Update settings.py

--- EON_Day_Ahead_Smart_Meter_Forecasting/settings.py
+++ EON_Day_Ahead_Smart_Meter_Forecasting/settings.py
@@ -1,18 +1,25 @@
 import os
 
 # region Paths
+from scripts.setup import ModelParameter
+
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 DIR_DATA = os.path.join(PROJECT_ROOT, 'data\\')
 DIR_MODEL = os.path.join(PROJECT_ROOT, 'model\\')
 DIR_SCRIPTS = os.path.join(PROJECT_ROOT, 'scripts\\')
-DIR_VALIDATION = os.path.join(PROJECT_ROOT, 'validation\\')
 
 DIR_PREPROCESSING = f"{DIR_DATA}preprocessing"
 DIR_SLIDING_WINDOW = f"{DIR_DATA}sliding_window"
+DIR_VALIDATION = f"{DIR_DATA}validation"
 
-ALL_MODELS = []
+ALL_MODELS = {
+    "default": ModelParameter(),
+    "patience": ModelParameter(patience=5)
+}
 
-ALL_SETUPS = []
+ALL_SETUPS = {
+
+}
 
 ACTUAL_SETUP =
 
