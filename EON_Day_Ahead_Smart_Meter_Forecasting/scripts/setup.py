91e9c279f9991e4da9a209b171aadef210a8cd3d
Thomas Kreidl <vanthomiy@live.de>
2022-05-04 18:43:34 +0200

    settings and setup

--- EON_Day_Ahead_Smart_Meter_Forecasting/scripts/setup.py
+++ EON_Day_Ahead_Smart_Meter_Forecasting/scripts/setup.py
@@ -18,25 +18,32 @@ class Timespan(Enum):
 
 
 class ModelParameter:
-    def __int__(self,
-                loss=tf.losses.MeanSquaredError(),
-                optimizer=tf.optimizers.Adam(),
-                metrics=[tf.metrics.MeanAbsolutePercentageError()],
-                max_epochs: int = 100,
-                patience: int = -1):
+    def __init__(self,
+                 loss=tf.losses.MeanSquaredError(),
+                 optimizer=tf.optimizers.Adam(),
+                 metrics=[tf.metrics.MeanAbsolutePercentageError()],
+                 max_epochs: int = 100,
+                 patience: int = -1):
         self.loss = loss
         self.optimizer = optimizer
         self.metrics = metrics
         self.max_epochs = max_epochs
         self.patience = patience
 
-    def name(self):
-        return f"ml{self.loss}_mo{self.optimizer}_mm{self.metrics}_me{self.max_epochs}_mp{self.patience}_"
+
+ALL_MODELS = {
+    "default": ModelParameter(),
+    "patience": ModelParameter(patience=5)
+}
 
 
 class Setup:
-    def __init__(self, normalization: Normalization, n_ahead: int, n_before: int, data_interval: Timespan,
-                 model_parameters: ModelParameter):
+    def __init__(self,
+                 model_parameters: ModelParameter = ALL_MODELS["default"],
+                 normalization: Normalization = Normalization.MEAN,
+                 n_ahead: int = 1,
+                 n_before: int = 2,
+                 data_interval: Timespan = Timespan.HOURLY):
         self.normalization = normalization
         """What normalization should be used? \"none\", \"mean\", \"01\""""
         self.n_ahead = n_ahead
@@ -50,6 +57,5 @@ class Setup:
         self.model_parameters = model_parameters
         """The model parameters from the model class"""
 
-    def name(self):
-        first = f"sn{self.normalization}_sa{self.n_ahead}_sb{self.n_before}_sw{self.window_size}_mi{self.data_interval}_"
-        return first + self.model_parameters.name()
+
+
